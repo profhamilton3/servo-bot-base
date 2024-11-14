5733b5dbb0ac2fc8a633226a83627061584a9fc0
T. Hamilton <profhamilton3@gmail.com>
2024-11-14 12:01:43 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+input.onButtonPressed(Button.A, function () {
+    wuKong.setServoSpeed(wuKong.ServoList.S0, 50)
+    wuKong.setServoSpeed(wuKong.ServoList.S2, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.stopAllMotor()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    wuKong.stopAllMotor()
+})
+wuKong.lightIntensity(100)
+wuKong.setLightMode(wuKong.LightMode.BREATH)
+wuKong.stopAllMotor()
+basic.forever(function () {
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+    basic.showNumber(input.rotation(Rotation.Pitch))
+    basic.showNumber(sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P8))
+    basic.showNumber(input.rotation(Rotation.Roll))
+})
